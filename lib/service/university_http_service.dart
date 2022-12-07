473d0fb58b4df413431ec966d95d17a9134c5777
ahmadshakir21 <ahmad.software.developer@gmail.com>
2022-12-07 13:28:36 +0300

    Another Commit

--- lib/service/university_http_service.dart
+++ lib/service/university_http_service.dart
@@ -6,6 +6,7 @@ import 'package:university/model/university_model.dart';
 
 class UniversityHttpService {
   //http://universities.hipolabs.com/search?country=iraq
+
   Uri uri = Uri(
       scheme: 'http',
       host: 'universities.hipolabs.com',
@@ -17,21 +18,6 @@ class UniversityHttpService {
     List result = json.decode(response.body) as List;
     List<UniversityModel> universityListModel =
         result.map((e) => UniversityModel.fromMap(e)).toList();
-
-    // Map result = json.decode(response.body) as Map;
-    // List listResult = result["data"] as List;
-    // List<UniversityModel> universityListModel =
-    //     listResult.map((e) => UniversityModel.fromMap(e)).toList();
-
-    // Post.fromJson(jsonDecode(response.body))
-
-    // List jsonResponse = json.decode(response.body);
-    // return jsonResponse
-    //     .map((data) => UniversityModelTest.fromJson(data))
-    //     .toList();
-
-    // var streetsFromJson = parsedJson['streets'];
-    // List<String> streetsList = new List<String>.from(streetsFromJson);
 
     return universityListModel;
   }
--- lib/view/home_screen.dart
+++ lib/view/home_screen.dart
@@ -3,40 +3,91 @@ import 'package:university/model/university_model.dart';
 import 'package:university/model/unversity_model_test.dart';
 import 'package:university/service/university_http_service.dart';
 
-class HomeScreen extends StatelessWidget {
+class HomeScreen extends StatefulWidget {
   HomeScreen({Key? key}) : super(key: key);
 
+  @override
+  State<HomeScreen> createState() => _HomeScreenState();
+}
+
+class _HomeScreenState extends State<HomeScreen> {
   UniversityHttpService universityHttpService = UniversityHttpService();
+
+  var items = [
+    'iraq',
+    'iran',
+    'poland',
+    'turkey',
+    'germany',
+    'italy',
+    'spain',
+    'egypt',
+    'tunsia',
+    'morroco',
+    'qatar',
+  ];
+
+  String dropDownValue = 'iraq';
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(),
-      body: FutureBuilder<List<UniversityModel>>(
-        future: universityHttpService.fetchUniversityList(),
-        builder: (context, snapshot) {
-          if (snapshot.hasError) {
-            return Center(
-              child: Text(snapshot.error.toString()),
-            );
-          } else if (snapshot.connectionState == ConnectionState.waiting) {
-            return const Center(
-              child: CircularProgressIndicator(),
-            );
-          } else if (snapshot.data == null) {
-            return const Center(
-              child: Text("No data"),
-            );
-          }
-          return ListView.builder(
-            itemCount: snapshot.data!.length,
-            itemBuilder: (context, index) {
-              final university = snapshot.data![index];
-
-              return ExpansionTile(title: Text(university.name));
+      body: Column(
+        children: [
+          DropdownButton(
+            value: dropDownValue,
+            icon: const Icon(Icons.keyboard_arrow_down),
+            items: items.map((String items) {
+              return DropdownMenuItem(
+                value: items,
+                child: Text(items),
+              );
+            }).toList(),
+            onChanged: (String? newValue) {
+              setState(() {
+                dropDownValue = newValue!;
+              });
             },
-          );
-        },
+          ),
+          // DropdownButton(
+          //     value: dropDownValue,
+          //     hint: const Text("Select Country"),
+          //     items: items
+          //         .map((String item) => DropdownMenuItem(child: Text(item)))
+          //         .toList(),
+          //     onChanged: (String value) {
+          //       setState(() {
+          //         dropDownValue = value;
+          //       });
+          //     }),
+          FutureBuilder<List<UniversityModel>>(
+            future: universityHttpService.fetchUniversityList(),
+            builder: (context, snapshot) {
+              if (snapshot.hasError) {
+                return Center(
+                  child: Text(snapshot.error.toString()),
+                );
+              } else if (snapshot.connectionState == ConnectionState.waiting) {
+                return const Center(
+                  child: CircularProgressIndicator(),
+                );
+              } else if (snapshot.data == null) {
+                return const Center(
+                  child: Text("No data"),
+                );
+              }
+              return ListView.builder(
+                itemCount: snapshot.data!.length,
+                itemBuilder: (context, index) {
+                  final university = snapshot.data![index];
+
+                  return ExpansionTile(title: Text(university.name));
+                },
+              );
+            },
+          ),
+        ],
       ),
     );
   }
